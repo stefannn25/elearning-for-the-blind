d63a3f541f4dda2164a07403b13e13bf40ff3764
Stephanie Caragan <159638561+stefannn25@users.noreply.github.com>
2025-12-09 21:13:09 +0800

    Refactor guest login session handling

--- guest_login.php
+++ guest_login.php
@@ -1,13 +1,8 @@
 <?php
 session_start();
-
-// Clear any student session
 session_unset();
-
-// Set guest session
 $_SESSION['guest'] = true;
 $_SESSION['fullname'] = "Guest User";
-
 header("Location: workspace.php");
 exit();
 ?>
